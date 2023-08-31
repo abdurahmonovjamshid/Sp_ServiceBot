bf830c57efc8940d3678df452aafa1b91e7d2dce
abdurahmonovjamshid <abdurahmonovjamshid060@gmail.com>
2023-08-31 11:36:09 +0500

    complete

--- bot/models.py
+++ bot/models.py
@@ -5,8 +5,14 @@ from data.models import Province
 
 
 class Tg_Users(models.Model):
+    LANG_CHOICES = (
+        ('uz', 'Uzbek'),
+        ('rus', 'Russian'),
+    )
+
     user_id = models.IntegerField()
     step = models.IntegerField(default=0)
+    lan = models.CharField(max_length=4, choices=LANG_CHOICES)
     number = models.IntegerField(blank=True, null=True)
     first_name = models.CharField(max_length=100, null=True, blank=True)
 
@@ -19,7 +25,7 @@ class Tg_Users(models.Model):
 
 
 class Orders(models.Model):
-    user = models.ForeignKey(Tg_Users, on_delete=models.SET_NULL, null=True, related_name='user_orders')
+    user = models.ForeignKey(Tg_Users, on_delete=models.CASCADE, null=True, related_name='user_orders')
     from_to = models.ForeignKey(Province, on_delete=models.SET_NULL, blank=True, null=True)
     where = models.ForeignKey(District, on_delete=models.SET_NULL, blank=True, null=True)
     seats = models.PositiveIntegerField(blank=True, null=True)
